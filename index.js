2a47aa210ddabb5f97dd549fb3eb331a46763e3c
Juliloio <julianaloiotile@gmail.com>
2020-06-23 12:45:51 -0300

    modificaciones para Heroku

--- index.js
+++ index.js
@@ -3,6 +3,9 @@
 // importa la libreria express
 const express = require('express');
 
+// ponemos el puerto del server en una variable
+const port = process.env.PORT || 3000;
+
 // crea el objeto app
 const app = express();
 
@@ -13,4 +16,4 @@ app.get('/', (req, res) => {
 });
 
 // el server escucha en el puerto 3000
-app.listen(3000);
+app.listen(port);
